184645e67aad1a16f9f315c9136fc84ca594b38d
Michael D. Hoyle <hoylemd@gmail.com>
2015-09-09 08:40:21 -0400

    add resourcess and text drawing

--- pysteroids/asteroids.py
+++ pysteroids/asteroids.py
@@ -1,5 +1,17 @@
 import pyglet
 game_window = pyglet.window.Window(800, 600)
 
+score_label = pyglet.text.Label(text="Score: 0", x=10, y=575)
+level_label = pyglet.text.Label(
+    text="Pysteroids", x=400, y=575, anchor_x='center')
+
+
+@game_window.event
+def on_draw():
+    game_window.clear()
+
+    level_label.draw()
+    score_label.draw()
+
 if __name__ == '__main__':
     pyglet.app.run()
---
+++ pysteroids/game/resources.py
@@ -0,0 +1,17 @@
+import pyglet
+pyglet.resource.path = ['../resources']
+pyglet.resource.reindex()
+
+
+def center_image(image):
+    """Sets an image's anchor point to it's center"""
+    image.anchor_x = image.width/2
+    image.anchor_y = image.height/2
+
+player_image = pyglet.resource.image('player.png')
+bullet_image = pyglet.resource.image('bullet.png')
+asteroid_image = pyglet.resource.image('asteroid.png')
+
+center_image(player_image)
+center_image(bullet_image)
+center_image(asteroid_image)
