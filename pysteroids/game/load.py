fe4b3f6b46773a3613ac7224adb67cec1ff597e5
Michael D. Hoyle <hoylemd@gmail.com>
2015-09-10 09:25:22 -0400

    mor constants

--- pysteroids/game/load.py
+++ pysteroids/game/load.py
@@ -2,6 +2,9 @@ import pyglet
 import random
 import resources
 import utils
+from settings import (
+    WINDOW_WIDTH, WINDOW_HEIGHT, LIVES_VERTICAL_OFFSET, LIVES_HORIZONTAL_OFFSET
+    )
 
 
 def asteroids(num_asteroids, player_position, batch=None):
@@ -11,8 +14,8 @@ def asteroids(num_asteroids, player_position, batch=None):
         asteroid_x, asteroid_y = player_position
 
         while utils.distance((asteroid_x, asteroid_y), player_position) < 100:
-            asteroid_x = random.randint(0, 800)
-            asteroid_y = random.randint(0, 600)
+            asteroid_x = random.randint(0, WINDOW_WIDTH)
+            asteroid_y = random.randint(0, WINDOW_HEIGHT)
 
         new_asteroid = pyglet.sprite.Sprite(
             img=resources.asteroid_image, x=asteroid_x, y=asteroid_y,
@@ -28,7 +31,8 @@ def player_lives(num_icons, batch=None):
     player_lives = []
     for i in range(num_icons):
         new_sprite = pyglet.sprite.Sprite(
-            img=resources.player_image, x=785-i*30, y=585, batch=batch)
+            img=resources.player_image, x=(LIVES_HORIZONTAL_OFFSET - i * 30),
+            y=LIVES_VERTICAL_OFFSET, batch=batch)
         new_sprite.scale = 0.5
         player_lives.append(new_sprite)
     return player_lives
