22f8c50b9117f4b72efa95c97b712929e1aee819
Michael D. Hoyle <hoylemd@gmail.com>
2015-09-10 09:07:25 -0400

    add settings file and use constants

--- pysteroids/asteroids.py
+++ pysteroids/asteroids.py
@@ -1,17 +1,23 @@
 import pyglet
 from game import resources, load
+from settings import (
+    WINDOW_WIDTH, WINDOW_HEIGHT, TOPBAR_VERTICAL_OFFSET,
+    WINDOW_HORIZONTAL_CENTER, WINDOW_VERTICAL_CENTER)
 
 
-game_window = pyglet.window.Window(800, 600)
+game_window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 main_batch = pyglet.graphics.Batch()
 
-score_label = pyglet.text.Label(text="Score: 0", x=10, y=575, batch=main_batch)
+score_label = pyglet.text.Label(
+    text="Score: 0", x=10, y=TOPBAR_VERTICAL_OFFSET, batch=main_batch)
 level_label = pyglet.text.Label(
-    text="Pysteroids", x=400, y=575, anchor_x='center', batch=main_batch)
+    text="Pysteroids", x=WINDOW_HORIZONTAL_CENTER, y=TOPBAR_VERTICAL_OFFSET,
+    anchor_x='center', batch=main_batch)
 
 player_ship = pyglet.sprite.Sprite(
-    img=resources.player_image, x=400, y=300, batch=main_batch)
+    img=resources.player_image, x=WINDOW_HORIZONTAL_CENTER,
+    y=WINDOW_VERTICAL_CENTER, batch=main_batch)
 asteroids = load.asteroids(3, player_ship.position, main_batch)
 
 lives = load.player_lives(3, main_batch)
