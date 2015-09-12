e03b80ecf577e42f0b1f70459d6cbe12c3ca2993
Michael D. Hoyle <mhoyle@freshbooks.com>
2015-09-12 19:53:38 -0400

    add degree normalization

--- pysteroids/game/utils.py
+++ pysteroids/game/utils.py
@@ -6,3 +6,19 @@ def distance(point_1=(0, 0), point_2=(0, 0)):
     return math.sqrt(
         (point_1[0] - point_2[0]) ** 2 +
         (point_1[1] - point_2[1]) ** 2)
+
+
+def normalize_degrees(angle):
+    magnitude = angle
+    direction = 1.0
+    if magnitude < 0:
+        direction = -1
+        magnitude *= direction
+    magnitude %= 360.0
+    magnitude *= direction
+
+    print magnitude
+    if magnitude < 0:
+        magnitude = 360.0 + magnitude
+
+    return magnitude
--- tests/util_test.py
+++ tests/util_test.py
@@ -10,6 +10,25 @@ def test_distance__y_only():
 
 
 def test_distance__diagonal():
-    print utils.distance((100.0, -3.2), (-0.750, 34.0))
     rounded = "{:.4f}".format(utils.distance((100.0, -3.2), (-0.750, 34.0)))
     assert rounded == '107.3983'
+
+
+def test_normalize_degrees__noop():
+    rounded = "{:.4f}".format(utils.normalize_degrees(97.5))
+    assert rounded == '97.5000'
+
+
+def test_normalize_degrees__negative():
+    rounded = "{:.4f}".format(utils.normalize_degrees(-134.234))
+    assert rounded == '225.7660'
+
+
+def test_normalize_degrees__positive_overflow():
+    rounded = "{:.4f}".format(utils.normalize_degrees(465.345))
+    assert rounded == '105.3450'
+
+
+def test_normalize_degrees__negative_overflow():
+    rounded = "{:.4f}".format(utils.normalize_degrees(-1785.0))
+    assert rounded == '15.0000'
