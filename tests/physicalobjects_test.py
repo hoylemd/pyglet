b3e261f1271d35b1290813e732d1e551e6a6b6b8
Michael D. Hoyle <mhoyle@freshbooks.com>
2015-09-13 15:32:39 -0400

    more tests

--- tests/physicalobjects_test.py
+++ tests/physicalobjects_test.py
@@ -3,7 +3,7 @@ from game.physicalobjects import InertialObject
 from tests.scaffolding import fixtures
 
 
-def test_init__motionless():
+def test_InertialObject_init__motionless():
     sut = InertialObject(img=fixtures['player_image'])
 
     assert sut.velocity_x == 0.0
@@ -11,7 +11,7 @@ def test_init__motionless():
     assert sut.rotation_speed == 0.0
 
 
-def test_loop_position__origin():
+def test_InertialObject_loop_position__origin():
     sut = InertialObject(img=fixtures['player_image'])
 
     assert sut.x == 0
@@ -23,7 +23,7 @@ def test_loop_position__origin():
     assert sut.y == 0
 
 
-def test_loop_position__on_screen():
+def test_InertialObject_loop_position__on_screen():
     sut = InertialObject(img=fixtures['player_image'])
 
     sut.x = 56.3
@@ -35,7 +35,7 @@ def test_loop_position__on_screen():
     assert sut.y == 174.23
 
 
-def test_loop_position__off_top():
+def test_InertialObject_loop_position__off_top():
     sut = InertialObject(img=fixtures['player_image'])
 
     sut.x = 56.3
@@ -47,7 +47,7 @@ def test_loop_position__off_top():
     assert sut.y == -25.0
 
 
-def test_loop_position__off_bottom():
+def test_InertialObject_loop_position__off_bottom():
     sut = InertialObject(img=fixtures['player_image'])
 
     sut.x = 56.3
@@ -59,7 +59,7 @@ def test_loop_position__off_bottom():
     assert sut.y == WINDOW_HEIGHT + 25.0
 
 
-def test_loop_position__off_left():
+def test_InertialObject_loop_position__off_left():
     sut = InertialObject(img=fixtures['player_image'])
 
     sut.x = -30.0
@@ -71,7 +71,7 @@ def test_loop_position__off_left():
     assert sut.y == 156.8
 
 
-def test_loop_position__off_right():
+def test_InertialObject_loop_position__off_right():
     sut = InertialObject(img=fixtures['player_image'])
 
     sut.x = WINDOW_WIDTH + 30.0
@@ -81,3 +81,111 @@ def test_loop_position__off_right():
 
     assert sut.x == -25.0
     assert sut.y == 156.8
+
+
+def test_InertialObject_update__motionless():
+    sut = InertialObject(img=fixtures['player_image'])
+
+    sut.x = 23.45
+    sut.y = 123.34
+
+    sut.velocity_x = 0.0
+    sut.velocity_y = 0.0
+
+    sut.update(1.0)
+
+    assert sut.x == 23.45
+    assert sut.y == 123.34
+
+
+def test_InertialObject_update__forward():
+    sut = InertialObject(img=fixtures['player_image'])
+
+    sut.x = 23.45
+    sut.y = 123.34
+
+    sut.velocity_x = 10.0
+    sut.velocity_y = 0.0
+
+    sut.update(1.0)
+
+    assert sut.x == 33.45
+    assert sut.y == 123.34
+
+
+def test_InertialObject_update__diagonal():
+    sut = InertialObject(img=fixtures['player_image'])
+
+    sut.x = 23.45
+    sut.y = 123.34
+
+    sut.velocity_x = 10.0
+    sut.velocity_y = -5.0
+
+    sut.update(1.0)
+
+    assert sut.x == 33.45
+    assert sut.y == 118.34
+
+
+def test_InertialObject_update__partial_dt():
+    sut = InertialObject(img=fixtures['player_image'])
+
+    sut.x = 23.45
+    sut.y = 123.34
+
+    sut.velocity_x = 10.0
+    sut.velocity_y = -5.0
+
+    sut.update(0.5)
+
+    assert sut.x == 28.45
+    assert sut.y == 120.84
+
+
+def test_InertialObject_update__motionless_rotation():
+    sut = InertialObject(img=fixtures['player_image'])
+
+    sut.rotation = 0.0
+
+    sut.rotation_speed = 0.0
+
+    sut.update(1.0)
+
+    assert sut.rotation == 0.0
+
+
+def test_InertialObject_update__positive_rotation():
+    sut = InertialObject(img=fixtures['player_image'])
+
+    sut.rotation = 0.0
+
+    sut.rotation_speed = 6.0
+
+    sut.update(1.0)
+
+    assert sut.rotation == 6.0
+
+
+def test_InertialObject_update__negative_rotation():
+    sut = InertialObject(img=fixtures['player_image'])
+
+    sut.rotation = 0.0
+
+    sut.rotation_speed = -15.0
+
+    sut.update(1.0)
+
+    assert sut.rotation == 345.0
+
+
+def test_InertialObject_update__rotation_partial_dt():
+    sut = InertialObject(img=fixtures['player_image'])
+
+    sut.rotation = 0.0
+
+    sut.rotation_speed = -10.0
+
+    sut.update(0.5)
+
+    assert sut.rotation == 355.0
