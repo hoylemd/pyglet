c0cf1811f7c94d4fe9aa6a6ed42eb6a683f7ff45
Michael D. Hoyle <hoylemd@gmail.com>
2015-09-14 11:17:39 -0400

    add key release tests

--- tests/player_test.py
+++ tests/player_test.py
@@ -10,6 +10,10 @@ def test_key_press__left():
 
     assert sut.keys['left']
 
+    sut.on_key_release(key.LEFT, None)
+
+    assert not sut.keys['left']
+
 
 def test_key_press__right():
     sut = Player(img=resources.player_image, x=200, y=300)
@@ -17,6 +21,10 @@ def test_key_press__right():
     sut.on_key_press(key.RIGHT, None)
 
     assert sut.keys['right']
+
+    sut.on_key_release(key.RIGHT, None)
+
+    assert not sut.keys['right']
 
 
 def test_key_press__up():
@@ -26,6 +34,10 @@ def test_key_press__up():
 
     assert sut.keys['up']
 
+    sut.on_key_release(key.UP, None)
+
+    assert not sut.keys['up']
+
 
 def test_key_press__down():
     sut = Player(img=resources.player_image, x=200, y=300)
@@ -33,3 +45,15 @@ def test_key_press__down():
     sut.on_key_press(key.DOWN, None)
 
     assert sut.keys['down']
+
+    sut.on_key_release(key.DOWN, None)
+
+    assert not sut.keys['down']
+
+
+def test_key_press__space():
+    sut = Player(img=resources.player_image, x=200, y=300)
+
+    sut.on_key_press(key.SPACE, None)
+
+    assert sut.signals['recenter']
