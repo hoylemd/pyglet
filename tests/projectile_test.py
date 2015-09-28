39504b2fac60c5ea6eca2d309d666fb32f37d97e
Michael D. Hoyle <hoylemd@gmail.com>
2015-09-28 11:47:57 -0400

    adda  test for default despawn time

--- tests/projectile_test.py
+++ tests/projectile_test.py
@@ -12,6 +12,14 @@ def test_init():
 
 
 @patch('game.projectile.clock')
+def test_init__dies_after_default_time(mock_clock):
+    sut = Projectile(name="bullet", img=resources.bullet_image,
+                     x=100.0, y=200.0)
+
+    mock_clock.schedule_once.assert_called_with(sut.die, 0.5)
+
+
+@patch('game.projectile.clock')
 def test_init__dies_after_set_time(mock_clock):
     sut = Projectile(name="bullet", img=resources.bullet_image,
                      x=100.0, y=200.0, lifespan=1.25)
