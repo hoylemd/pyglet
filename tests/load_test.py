89f1a944c309c5a65ca19ec68519a37b4e46bc8f
Michael D. Hoyle <mhoyle@freshbooks.com>
2015-09-28 13:26:53 -0400

    a test for loading the player

--- tests/load_test.py
+++ tests/load_test.py
@@ -1,15 +1,30 @@
-from game import load
-from game import utils
+from game import load, utils
+from settings import WINDOW_HORIZONTAL_CENTER, WINDOW_VERTICAL_CENTER
 
 
 def test_asteroids__3():
-    results = load.asteroids(3, (100, 100))
+    suts = load.asteroids(3, (100, 100))
 
-    assert len(results) == 3
+    assert len(suts) == 3
 
 
 def test_asteroids__20_no_collosions():
-    results = load.asteroids(3, (100, 100))
+    suts = load.asteroids(3, (100, 100))
 
-    for result in results:
-        assert utils.distance(result.position, (100, 100)) >= 100
+    for sut in suts:
+        assert utils.distance(sut.position, (100, 100)) >= 100
+
+
+def test_player_ship():
+    sut = load.player_ship()
+
+    assert sut.thrust == 200.0
+    assert sut.maneuvering_thrust == 360.0
+
+    assert sut.weapon_projectile_speed == 700.0
+
+    assert sut.x == WINDOW_HORIZONTAL_CENTER
+    assert sut.y == WINDOW_VERTICAL_CENTER
+
+    assert sut.center_x == WINDOW_HORIZONTAL_CENTER
+    assert sut.center_y == WINDOW_VERTICAL_CENTER
