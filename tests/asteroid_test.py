dd75d4ddb65744c5a68ec2cd2fe39bb3bf5c1c43
Michael D. Hoyle <hoylemd@gmail.com>
2015-09-28 16:59:07 -0400

    add stubs for asteroid tests

--- tests/asteroid_test.py
+++ tests/asteroid_test.py
@@ -7,3 +7,11 @@ def test_init():
     sut = Asteroid(name="Asteroid", img=resources.asteroid_image)
 
     assert sut is not None
+
+
+def test_handle_collision__full_size():
+    raise NotImplementedError
+
+
+def test_handle_collision__quarter_size():
+    raise NotImplementedError
