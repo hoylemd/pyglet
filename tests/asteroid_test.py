260094aba11f4b18622ccfd00f5dfd2870e76ed7
Michael D. Hoyle <mhoyle@freshbooks.com>
2015-09-28 21:08:12 -0400

    add tests for asteroid collisions

--- tests/asteroid_test.py
+++ tests/asteroid_test.py
@@ -1,6 +1,16 @@
 from game.asteroid import Asteroid
+from tests.physicalobjects_test import Collider
 
 from game import resources
+
+
+def assert_in_range(value, minimum, maximum):
+    if minimum > maximum:
+        temp = maximum
+        minimum = maximum
+        minimum = temp
+
+    assert value >= minimum and value <= maximum
 
 
 def test_init():
@@ -10,8 +20,23 @@ def test_init():
 
 
 def test_handle_collision__full_size():
-    raise NotImplementedError
+    sut = Asteroid(name="Asteroid", img=resources.asteroid_image, x=5.0, y=5.0)
+    collider = Collider(x=5.0, y=5.0)
+
+    sut.handle_collision(collider)
+
+    assert sut.dead
+    assert_in_range(len(sut.new_objects), 2, 3)
+    for obj in sut.new_objects:
+        assert obj.scale == 0.5
 
 
 def test_handle_collision__quarter_size():
-    raise NotImplementedError
+    sut = Asteroid(name="Asteroid", img=resources.asteroid_image, x=5.0, y=5.0)
+    sut.scale = 0.25
+    collider = Collider(x=5.0, y=5.0)
+
+    sut.handle_collision(collider)
+
+    assert sut.dead
+    assert len(sut.new_objects) == 0
